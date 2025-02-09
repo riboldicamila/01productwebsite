f4d75419bd946fb64f3d5b2ccb1d37086bdb46d2
riboldicamila <cami.riboldi@gmail.com>
2025-02-09 16:03:26 +0100

    content

--- src/Pages/Home/Collection.jsx
+++ src/Pages/Home/Collection.jsx
@@ -2,10 +2,41 @@ import React, { useState } from "react";
 
 import "./CollectionPage.css";
 import GenericButton from "../../Components/Button";
-import { products } from "../../Data/CollectionData";
+
+import react from "../../Components/Images/react.jpg";
+import html from "../../Components/Images/html.jpg";
+import javascript from "../../Components/Images/js.png";
+import css from "../../Components/Images/css.png";
+import python from "../../Components/Images/python.jpg";
+import sql from "../../Components/Images/sql.png";
+import git from "../../Components/Images/git.png";
+import aws from "../../Components/Images/aws.png";
+import oop from "../../Components/Images/oop.jpg";
+import jira from "../../Components/Images/jira.png";
+import cicd from "../../Components/Images/cdci.png";
+import scrum from "../../Components/Images/scrum.png";
+
+
 
 const CollectionPage = () => {
   const [viewMode, setViewMode] = useState("grid");
+
+  const products = [
+    { id: 1, name: 'React Js', rating: 5, image: react },
+    { id: 2, name: 'HTML', rating: 5, image: html },
+    { id: 3, name: 'JavaScript', rating: 5, image: javascript },
+    { id: 4, name: 'CSS', rating: 4, image: css },
+    { id: 5, name: 'Python', rating: 4, image: python },
+    { id: 6, name: 'SQL', rating: 4, image: sql },
+    { id: 7, name: 'Git', rating: 5, image: git },
+    { id: 8, name: 'AWS Certification', rating: 5, image: aws },
+    { id: 10, name: 'Object Oriented Programming', rating: 5, image: oop },
+    { id: 11, name: 'Scrum', rating: 4, image: scrum },
+    { id: 13, name: 'JIRA', rating: 4, image: jira },
+    { id: 14, name: 'CI/CD', rating: 3, image: cicd },
+
+  ];
+
 
   return (
     <div className="collection-container">
