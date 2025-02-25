d8f56f1793215ec2a281d8a10d6abe350445ecf5
riboldicamila <cami.riboldi@gmail.com>
2025-02-25 11:30:38 +0100

    content update

--- src/Pages/Home/Search.jsx
+++ src/Pages/Home/Search.jsx
@@ -41,6 +41,24 @@ const Search = () => {
         </div>
       ),
     },
+    {
+      question:
+        "Check out my walkthrough of a MERN (MongoDB, Express, React.js, Node.js) project",
+      answer: (
+        <div className="video-container">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/NpYvNChbdcw?si=ay7zXsyjNsJ3finI"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        </div>
+      ),
+    },
   ];
 
   const logos = ["React Js", "HTML", "JavaScript", "CSS", "Git", "SQL"];
