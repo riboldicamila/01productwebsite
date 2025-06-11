92d9c1141fc75c85b793cd4d4521756aef0ae501
riboldicamila <cami.riboldi@gmail.com>
2025-06-11 19:05:59 +0200

    content update

--- src/Data/ContactData.js
+++ src/Data/ContactData.js
@@ -1,7 +1,7 @@
 
 export const CONTACT_INFO = {
   address: [
-    "Worldwide. Argentina and Italy based."
+    "Worldwide. From Argentina, currently based in Italy."
   ],
   email: "cami.riboldi@gmail.com",
   additionalInfo: "Reach out leaving an email, or connect on Linkedin.",
--- src/Data/HomeData.js
+++ src/Data/HomeData.js
@@ -1,6 +1,6 @@
-import portfolio01 from "../assets/Images/portfolio01.png"
+import portfolio01 from "../assets/Images/portfolio01.png";
 
-export const MOCK_DATA = { 
+export const MOCK_DATA = {
   title: "Let's Build!",
   description:
     "Creating web applications that are functional, responsive, and easy to use. Turn your ideas into reality.",
@@ -32,70 +32,70 @@ export const PROJECTS = [
     title: "Law Firm Website",
     date: "12 April 2025",
     author: "Camila Riboldi",
-    subtitle: "Designed and developed the website for the law firm “Dra. Claudia Simón & Asoc. | Estudio Jurídico” using React.js and Tailwind CSS. Focused on creating a modern, responsive, and user-friendly interface that reflects the professionalism and values of the firm.",
-    image:"https://res.cloudinary.com/dav7tzdzv/image/upload/v1746778657/Copy_of_Copy_of_Patagonia_1_ioqj4a.jpg",
+    subtitle:
+      "Designed and developed the website for the law firm “Dra. Claudia Simón & Asoc. | Estudio Jurídico” using React.js and Tailwind CSS. Focused on creating a modern, responsive, and user-friendly interface that reflects the professionalism and values of the firm.",
+    image:
+      "https://res.cloudinary.com/dav7tzdzv/image/upload/v1746778657/Copy_of_Copy_of_Patagonia_1_ioqj4a.jpg",
     slug: "law-website",
-    url: "https://claudiasimonyasociados-estudiojuridico.vercel.app/"
+    url: "https://claudiasimonyasociados-estudiojuridico.vercel.app/",
   },
   {
     id: 2,
     title: "Patagonia Website",
     date: "08 Feb 2025",
     author: "Camila Riboldi",
-    subtitle: "A website project built with Node.js, Express, MongoDB, React.js, Axios, and Tailwind.",
-    image:"https://res.cloudinary.com/dav7tzdzv/image/upload/v1740142550/7c2fca4a-7ba5-4b9d-af35-f758db053a37.png",
+    subtitle:
+      "A website project built with Node.js, Express, MongoDB, React.js, Axios, and Tailwind.",
+    image:
+      "https://res.cloudinary.com/dav7tzdzv/image/upload/v1740142550/7c2fca4a-7ba5-4b9d-af35-f758db053a37.png",
     slug: "patagonia-website",
-    url: "https://patagonia-backend.onrender.com/"
+    url: "https://patagonia-backend.onrender.com/",
   },
   {
     id: 2,
     title: "Pampa Bakery Website",
     date: "28 Jan 2025",
     author: "Camila Riboldi",
-    subtitle: "A completed project for a bakery website using React.js, Redux for global state management, and Firebase.",
-    image:"https://res.cloudinary.com/dav7tzdzv/image/upload/v1739806305/E-commerce_Sample_qmii38.png",
+    subtitle:
+      "A completed project for a bakery website using React.js, Redux for global state management, and Firebase.",
+    image:
+      "https://res.cloudinary.com/dav7tzdzv/image/upload/v1739806305/E-commerce_Sample_qmii38.png",
     slug: "daily-panel-app",
-    url: "https://pampa-bakery.vercel.app/"
+    url: "https://pampa-bakery.vercel.app/",
   },
-  // {
-  //   id: 3,
-  //   title: "Minimal Web Design - React Practice",
-  //   date: "02 Jan 2025",
-  //   author: "Camila Riboldi",
-  //   subtitle: "A minimalist web page created with React, CSS, and Bootstrap, focusing on clean UI design and smooth user interactions.",
-  //   image: "https://res.cloudinary.com/dav7tzdzv/image/upload/v1739805391/Copy_of_personal_website_tenfxj.png",
-  //   slug: "minimal-web-design",
-  //   url: "https://minimalistic-web-sample.vercel.app/",
-  // },
+  {
+    id: 5,
+    title: "Sabai Thai - Fullstack MERN Website",
+    date: "06 April 2025",
+    author: "Camila Riboldi",
+    subtitle:
+      "A fullstack restaurant website built with the MERN stack (MongoDB, Express.js, React, Node.js), featuring user-friendly design, animations, and responsive layouts.",
+    image:
+      "https://res.cloudinary.com/dav7tzdzv/image/upload/v1749660777/paragonia_website_oki48k.png",
+    slug: "sabai-thai",
+    url: "https://sabaithai.vercel.app/",
+  },
   {
     id: 4,
     title: "Swiss Media - Responsive Website Design",
     date: "05 Mar 2023",
     author: "Camila Riboldi",
-    subtitle: "A responsive website built with HTML, CSS, Sass, and Bootstrap, using various libraries to enhance interactivity and animation.",
-    image: "https://res.cloudinary.com/dav7tzdzv/image/upload/v1739806539/E-commerce_Sample_1_mqbnxi.png",
+    subtitle:
+      "A responsive website built with HTML, CSS, Sass, and Bootstrap, using various libraries to enhance interactivity and animation.",
+    image:
+      "https://res.cloudinary.com/dav7tzdzv/image/upload/v1739806539/E-commerce_Sample_1_mqbnxi.png",
     slug: "swiss-media",
     url: "https://riboldicamila.github.io/swissmedia-basic-html-saas/",
-
   },
   {
     id: 5,
     title: "Corporate Projects",
     date: "15 Nov 2025",
     author: "Camila Riboldi",
-    subtitle: "I’ve had the chance to work at companies like JP Morgan and ExxonMobil where I worked on frontend and backend development, building applications.",
+    subtitle:
+      "I’ve had the chance to work at companies like JP Morgan and ExxonMobil where I worked on frontend and backend development, building applications.",
     image: portfolio01,
     slug: "corporate-projects",
     url: "https://riboldi-camila-sw.vercel.app/",
-  },
-  {
-    id: 2,
-    title: "Projects & Portfolio",
-    date: "10 Jun 2024",
-    author: "Camila Riboldi",
-    subtitle: "A landing page built with React, incorporating Material UI, GSAP animations, and dynamic routing to enhance user experience.",
-    image: "https://res.cloudinary.com/dav7tzdzv/image/upload/v1739806813/E-commerce_Sample_2_cryfkp.png",
-    slug: "projects-portfolio",
-    url: "https://camila-riboldi.vercel.app/"
   },
 ];
